8e4214814eb200d78f2a0402bd737bac98fdb6ca
Konstantin Tokarev <annulen@yandex.ru>
2019-06-29 17:18:19 +0300

    Import files from libxml2 recipe to be used as a template

---
+++ README.md
@@ -0,0 +1,12 @@
+## Conan package recipe for [*libxslt*](https://xmlsoft.org/XSLT/)
+
+libxslt is a software library implementing XSLT processor, based on libxml2. It also implements most of the EXSLT set
+of processor-portable extensions functions and some of Saxon's evaluate and expressions extensions.
+
+
+## Conan Recipe License
+
+NOTE: The conan recipe license applies only to the files of this recipe, which can be used to build and package libxml2.
+It does *not* in any way apply or is related to the actual software being packaged.
+
+[MIT](https://github.com/qtwebkit/conan-libxslt/blob/stable/1.1.33/LICENSE.md)
---
+++ build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import copy
+import platform
+
+from bincrafters import build_template_default
+
+if __name__ == "__main__":
+
+    builder = build_template_default.get_builder(pure_c=True)
+
+    items = []
+    for item in builder.items:
+        # add msys2 and mingw as a build requirement for mingw builds
+        if platform.system() == "Windows" and item.settings["compiler"] == "gcc" and \
+                not item.options.get("libxslt:shared", False):
+            new_build_requires = copy.copy(item.build_requires)
+            new_build_requires["*"] = new_build_requires.get("*", []) + \
+                ["mingw_installer/1.0@conan/stable",
+                 "msys2_installer/latest@bincrafters/stable"]
+            items.append([item.settings, item.options, item.env_vars,
+                          new_build_requires, item.reference])
+        else:
+            items.append(item)
+    builder.items = items
+
+    builder.run()

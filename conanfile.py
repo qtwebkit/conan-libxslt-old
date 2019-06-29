bf4285b62a443fe598246d63617ba9810e165bcd
Konstantin Tokarev <annulen@yandex.ru>
2019-06-30 02:52:04 +0300

    Add XML_PATH

--- conanfile.py
+++ conanfile.py
@@ -120,7 +120,9 @@ class LibxsltConan(ConanFile):
         if self.settings.os == "iOS" and self.settings.arch == "x86_64":
             build = False
 
-        env_build.configure(args=configure_args, build=build, configure_dir=self._full_source_subfolder)
+        xml_config = tools.unix_path(self.deps_cpp_info["libxml2"].rootpath) + "/bin/xml2-config"
+
+        env_build.configure(args=configure_args, build=build, configure_dir=self._full_source_subfolder, vars={"XML_CONFIG": xml_config})
         env_build.make(args=["install"])
 
     def package(self):

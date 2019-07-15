53a517df25b72ecfa7f2265b64d3b4db7530a22b
SSE4 <tomskside@gmail.com>
2019-07-15 21:40:28 +0200

    - simplify build.py
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -12,17 +12,7 @@ if __name__ == "__main__":
     items = []
     for item in builder.items:
         if item.options["libxslt:shared"] == True:
-            # add msys2 and mingw as a build requirement for mingw builds
-            if platform.system() == "Windows" and item.settings["compiler"] == "gcc" and \
-                    not item.options.get("libxslt:shared", False):
-                new_build_requires = copy.copy(item.build_requires)
-                new_build_requires["*"] = new_build_requires.get("*", []) + \
-                    ["mingw_installer/1.0@conan/stable",
-                     "msys2_installer/latest@bincrafters/stable"]
-                items.append([item.settings, item.options, item.env_vars,
-                              new_build_requires, item.reference])
-            else:
-                items.append(item)
+            items.append(item)
     builder.items = items
 
     builder.run()

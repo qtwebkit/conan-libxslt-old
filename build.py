f6b3b67f15b8ecaf82a30d139fdf241bbc698fe3
SSE4 <tomskside@gmail.com>
2019-07-18 11:12:54 +0200

    - update from templates
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import copy
-import platform
+
 
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=True)
-
-    items = []
-    for item in builder.items:
-        if item.options["libxslt:shared"] == True:
-            items.append(item)
-    builder.items = items
+    builder = build_template_default.get_builder()
 
     builder.run()
